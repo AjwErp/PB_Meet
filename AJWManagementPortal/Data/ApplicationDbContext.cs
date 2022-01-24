62da4a06986a8bdb421cf87045c1fac8c0cbdf3e
Yousuf Rakib <44175763+YousufRakib@users.noreply.github.com>
2022-01-25 04:00:57 +0600

    Add monthly and yearly ledger (25-01-2022)

--- AJWManagementPortal/Data/ApplicationDbContext.cs
+++ AJWManagementPortal/Data/ApplicationDbContext.cs
@@ -25,6 +25,8 @@ namespace AJWManagementPortal.Data
         public DbSet<DailyPlanOut> DailyPlanOut { get; set; }
         public DbSet<DailyPlanInAccounts> DailyPlanInAccountss { get; set; }
         public DbSet<DailyPlanOutAccounts> DailyPlanOutAccountss { get; set; }
+        public DbSet<MonthlyGeneralLedgerBook> MonthlyGeneralLedgerBook { get; set; }
+        public DbSet<YearlyGeneralLedgerBook> YearlyGeneralLedgerBook { get; set; }
         public DbSet<Img> Img { get; set; }
         public DbSet<AccountDailyContinueSheetM> DailyContinueSheetMs { get; set; }
         public DbSet<aDailyCash> aDailyCashes { get; set; }
---
+++ AJWManagementPortal/Models/ViewModels/GeneralLedgerBookListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJWManagementPortal.Models.ViewModels
+{
+    public class GeneralLedgerBookListViewModel
+    {
+        public IEnumerable<YearlyGeneralLedgerBook> YearlyGeneralLedgerBooks;
+        public IEnumerable<MonthlyGeneralLedgerBook> MonthlyGeneralLedgerBooks;
+        //public List<YearlyGeneralLedgerBook> YearlyGeneralLedgerBooks { get; set; }
+        //public List<MonthlyGeneralLedgerBook> MonthlyGeneralLedgerBooks { get; set; }
+    }
+}
