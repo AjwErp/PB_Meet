06512ac9d379596326000b1e015a8af636039201
AjwErp <ajwagro@gmail.com>
2022-02-09 14:34:51 +0500

    Store

---
+++ AJWManagementPortal/Areas/Store/Controllers/MaterialLedgerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJWManagementPortal.Areas.Store.Controllers
+{
+    [Area("Store")]
+    public class MaterialLedgerController : Controller
+    {
+        public IActionResult MaterialStockLedgerList()
+        {
+            return View();
+        }
+
+        public IActionResult ElectricStockLedgerList()
+        {
+            return View();
+        }
+        public IActionResult VachicleMaterialStockLedgerList()
+        {
+            return View();
+        }
+
+        public IActionResult StockVoucherViaScrabLedgerList()
+        {
+            return View();
+        }
+    }
+}
