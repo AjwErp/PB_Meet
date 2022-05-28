2fcf590912bf24e0a33e85d7db6f4a7fa251159a
AjwErp <ajwagro@gmail.com>
2022-05-28 09:25:01 +0500

    28-5-2022

--- AJWManagementPortal/ViewModels/DgmAccountsDepartmentReportsListViewModel.cs
+++ AJWManagementPortal/ViewModels/DgmAccountsDepartmentReportsListViewModel.cs
@@ -10,6 +10,13 @@ namespace AJWManagementPortal.ViewModels
     {
         public IEnumerable<MonthlyAcountReportsViewModel> MonthlyAcountReportsViewModel { get; set; }
         public IEnumerable<aDailyCash> aDailyCash { get; set; }
+        //for Daily Accounts Reports in DGM Office
+        //public IEnumerable<aDailyCash> aDailyCashes { get; set; }
+
+        public IEnumerable<DailyMeezan> DailyMeezan { get; set; } 
+        public IEnumerable<MeezanBankIEReport> MeezanBankIEReport { get; set; }
+
+        public IEnumerable<DailySuppliersCashTransactionReport> DailySuppliersCashTransactionReportliers { get; set; }
 
     }
 }
